a3b4f098ce266b1746e9799fb1ec7afe1ea36e3e
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2016-11-25 16:49:06 -0500

    Returns next available incremental AD computer name
    If AD contains "D001" through "D055", function call (e.g. -Prefix "D" -Digits 3) returns "D056"

---
+++ Get-NextAdComputerName.ps1
@@ -0,0 +1,16 @@
+﻿function Get-NextAdComputerName {
+    param ($Prefix, $Digits)
+    $clist = Get-ADComputer -Filter * -Properties Name -ErrorAction SilentlyContinue| 
+        ?{$_.Name -match "$Prefix[0-9]{$Digits}"} | 
+            Select -ExpandProperty Name |
+                Sort Name
+    if ($clist.Count -gt 0) {
+        $n = $clist.Count
+        [int]$x = [convert]::ToInt32($clist[$n-1].Replace($Prefix,""),10)
+        [string]$next = $x + 1
+        while ($next.Length -lt $Digits) {
+            $next = "0" + $next
+        }
+    }
+    $($Prefix + $next)
+}
