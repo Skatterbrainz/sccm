7e74886b47592c162e6f78a95b482584c7aa0012
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-12-07 01:10:26 -0500

    Multiple updates
    Changed source URI, and parsing code.  Added comments for entertainment value.

--- Get-CmFullVersionName.ps1
+++ Get-CmFullVersionName.ps1
@@ -15,23 +15,34 @@ function Get-CmFullVersionName {
 	.OUTPUT
 		String containing full version name, or $null if no match
 	#>
+    [CmdletBinding()]
     param (
-        [parameter(Mandatory=$True, HelpMessage="Build Number")]
+        [parameter(Mandatory=$True, ValueFromPipeline=$True, HelpMessage="ConfigMgr Build Number")]
         [ValidateNotNullOrEmpty()]
         [string] $BuildNumber
     )
     $uri = 'https://www.systemcenterdudes.com/sccm-2012-version-numbers/'
-    $raw = (New-Object System.Net.WebClient).DownloadString($uri)
+    try {
+        Write-Verbose "Reading: $uri"
+        $raw = (New-Object System.Net.WebClient).DownloadString($uri)
+        Write-Verbose "HTML data source imported successfully"
+    }
+    catch {
+        Write-Error $_.Exception.HelpMessage
+        break
+    }
     $raw = $raw.Replace("`n"," ")
-
+    # get rough starting point to snatch table data
     $start = $raw.IndexOf('class="tablepress tablepress-id-1">')
     $content = $raw.Substring($start+42)
+    # get ending point to trim off excess trailing data
     $end = $content.IndexOf('</tbody>')
+    # trim data to get just the desired table, oh, that big juicy table
     $content = $content.Substring(0, $end)
     $data = $content -replace "</td>",""
     $data = $data -replace "</tr>",""
     $data = $data -split "td class"
-
+    # you're probably laughing right about here, but that's okay
     foreach ($row in $data) {
         if ($row.StartsWith('="column-1"')) {
             $name = ($row.Substring(12)) -replace '<', ''
@@ -39,9 +50,12 @@ function Get-CmFullVersionName {
         elseif ($row.StartsWith('="column-2"')) {
             $ver = ($row.Substring(12)) -replace '<', ''
         }
+        # okay, by now, you're probably angry, that's okay too
+        Write-Verbose "NAME: $name // VERSION: $ver"
         if ($BuildNumber -eq $ver) {
             Write-Output $name
             break
         }
     }
+    # phew! you made it!
 }
