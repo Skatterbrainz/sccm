9fb6e6ef792454dc81ddd7f8275b7f2fb4e8fb3a
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-10-02 09:47:37 -0400

    Update Invoke-PhasedUpgrade.ps1

--- Invoke-PhasedUpgrade.ps1
+++ Invoke-PhasedUpgrade.ps1
@@ -57,14 +57,12 @@ $initParams = @{}
 #$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
 #$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
 
-# Do not change anything below this line
+# Do not change the following block of lines
 
 # Import the ConfigurationManager.psd1 module 
 if((Get-Module ConfigurationManager) -eq $null) {
     Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
 }
-
-# Connect to the site's drive if it is not already present
 if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
     New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $SiteServer @initParams
 }
