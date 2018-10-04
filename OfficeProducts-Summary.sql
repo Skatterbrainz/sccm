42372d663ea804950eecf0c0008a4d56a1a4a4f6
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-10-04 17:18:16 -0400

    Create OfficeProducts-Summary.sql

---
+++ OfficeProducts-Summary.sql
@@ -0,0 +1,16 @@
+--https://docs.microsoft.com/en-us/officeupdates/update-history-office365-proplus-by-date
+SELECT DISTINCT 
+	dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED.ProductName0, 
+	COUNT(*) AS Installs
+FROM 
+	dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED 
+WHERE 
+	(dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED.ProductName0 IN 
+	('Microsoft Office 365 ProPlus - en-us',
+	'Microsoft Project Standard 2016 - en-us',
+	'Microsoft Project Professional 2016 - en-us',
+	'Microsoft Visio Standard 2016 - en-us',
+	'Microsoft Visio Professional 2016 - en-us')
+)
+GROUP BY ProductName0
+ORDER BY ProductName0
