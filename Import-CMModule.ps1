f79ce192adca533a70089309aab1f6ece1b6e98d
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-10-30 16:59:13 -0400

    Create Import-CMModule.ps1

---
+++ Import-CMModule.ps1
@@ -0,0 +1,16 @@
+if (!(Get-Module -Name "ConfigurationManager")) {
+    if ([string]::IsNullOrEmpty($env:SMS_ADMIN_UI_PATH)) {
+        Write-Warning "ConfigMgr Console needs to be installed on the computer where this script is used"
+        break
+    }
+    $mpath = $(Get-Item $($env:SMS_ADMIN_UI_PATH)).Parent.FullName
+    try {
+        Import-Module $(Join-Path -Path $mpath -ChildPath "ConfigurationManager.psd1") -ErrorAction Stop
+        Write-Verbose "configuration manager powershell module loaded successfully"
+    }
+    catch {
+        Write-Error $Error[0].Exception.Message
+        break
+    }
+}
+(!(!(Get-Module -Name "ConfigurationManager")))
