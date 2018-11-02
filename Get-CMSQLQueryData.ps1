65d1ea1a9fbe34107a97eff9f87bf7ee20cf5abc
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-11-02 15:40:20 -0400

    Update Get-CMSQLQueryData.ps1

--- Get-CMSQLQueryData.ps1
+++ Get-CMSQLQueryData.ps1
@@ -18,6 +18,9 @@
     .\Get-CMSQLQueryData.ps1 -AdoConnection $conn -Query "select * from dbo.v_R_System where Name0='DT123" -SiteCode "P01"
     .\Get-CMSQLQueryData.ps1 -AdoConnection $conn -Query "select * from dbo.v_R_System where Name0='DT456" -SiteCode "P01"
     $conn.Close()
+.EXAMPLE
+    $query = Get-Content -Path "c:\queries\cm_devices.sql" | Out-String
+    $data = .\Get-CMSQLQueryData.ps1 -Query $query -SQLServerName "cm01.contoso.local" -SiteCode "HHQ"
 
 .NOTES
     1.0.0 - DS - initial release
@@ -25,16 +28,16 @@
 
 [CmdletBinding()]
 param (
+    [parameter(Mandatory=$False, ValueFromPipeline=$True, HelpMessage="SQL Query Statement")]
+        [ValidateNotNullOrEmpty()]
+        [string] $Query,
     [parameter(Mandatory=$False, HelpMessage="SQL Server ADO Connection Object")]
-    $AdoConnection,
-    [parameter(Mandatory=$True, HelpMessage="SQL Query Statement")]
-    [ValidateNotNullOrEmpty()]
-    [string] $Query,
+        $AdoConnection,
     [parameter(Mandatory=$False, HelpMessage="ConfigMgr SQL Server Host Name")]
-    [string] $SQLServerName,
+        [string] $SQLServerName,
     [parameter(Mandatory=$True, HelpMessage="ConfigMgr Site Code")]
-    [ValidateNotNullOrEmpty()]
-    [string] $SiteCode
+        [ValidateNotNullOrEmpty()]
+        [string] $SiteCode
 )
 $DatabaseName = "CM_$SiteCode"
 if (!$AdoConnection) {
