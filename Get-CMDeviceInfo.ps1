c143c19b2f3b7ac2b00bebb6a09406bf14934693
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-11-06 22:01:48 -0500

    Update Get-CMDeviceInfo.ps1

--- Get-CMDeviceInfo.ps1
+++ Get-CMDeviceInfo.ps1
@@ -12,6 +12,7 @@
     $data = .\Get-CMDeviceInfo.ps1 -ServerName "cm01.contoso.local" -SiteCode "P01" -ComputerNames "DT123","DT456"
 .NOTES
     1.0.1 - DS - Initial release
+    1.0.2 - DS - fixed SQL query to correct outer join issue
 #>
 
 [CmdletBinding()]
@@ -26,14 +27,22 @@ param (
         [ValidateNotNullOrEmpty()]
         [string[]] $ComputerNames
 )
+Write-Verbose "----------------------------------------------"
 Write-Verbose "(Get-CmDeviceInfo - getting configmgr data)"
 $DatabaseName = "CM_$SiteCode"
 Write-Verbose "database name is $DatabaseName"
 $queryBase = @"
 SELECT DISTINCT 
-	dbo.vWorkstationStatus.Name, 
-	dbo.vWorkstationStatus.UserName,
-	dbo.v_GS_OPERATING_SYSTEM.Caption0 AS OperatingSystem, 
+	dbo.v_R_System.Name0, 
+	dbo.v_R_System.ResourceID, 
+	dbo.v_R_System.AD_Site_Name0 AS [ADSite], 
+	dbo.vWorkstationStatus.ClientVersion, 
+	dbo.vWorkstationStatus.LastHardwareScan AS [LastHwScan], 
+    DATEDIFF(dd,dbo.vWorkstationStatus.LastHardwareScan,GETDATE()) AS InvAge,
+    dbo.vWorkstationStatus.LastPolicyRequest,
+    dbo.vWorkstationStatus.LastHealthEvaluationResult AS [LastHealthEval],  
+	dbo.v_GS_COMPUTER_SYSTEM.Model0 AS [Model], 
+	dbo.v_GS_OPERATING_SYSTEM.Caption0 AS [OperatingSystem], 
 	CASE 
 		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 10586) THEN '1511' 
 		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 14393) THEN '1607' 
@@ -44,33 +53,32 @@ SELECT DISTINCT
 		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 7601) THEN 'SP1' 
 		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 9600) THEN 'RTM' 
 		ELSE dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 END AS OsBuild, 
+    dbo.vWorkstationStatus.LastDDR, 
 	dbo.vWorkstationStatus.SystemType, 
-	dbo.vWorkstationStatus.ClientVersion, 
-	dbo.vWorkstationStatus.UserDomain, 
-	dbo.v_R_System.AD_Site_Name0 AS ADSite, 
-	dbo.v_GS_COMPUTER_SYSTEM.Model0 AS Model, 
-	dbo.v_GS_SYSTEM_ENCLOSURE.SerialNumber0 AS SerialNumber, 
 	CASE 
 		WHEN (dbo.vWorkstationStatus.IsVirtualMachine = 1) THEN 'Y' 
 		ELSE 'N' END AS IsVM, 
-	dbo.vWorkstationStatus.LastHealthEvaluationResult AS LastHealthEval, 
-	dbo.vWorkstationStatus.LastHardwareScan AS LastHwScan, 
-	DATEDIFF(dd,dbo.vWorkstationStatus.LastHardwareScan,GETDATE()) AS InvAge,
-	dbo.vWorkstationStatus.LastDDR
+	dbo.vWorkstationStatus.UserName, 
+	dbo.vWorkstationStatus.UserDomain, 
+	dbo.v_GS_SYSTEM_ENCLOSURE.SerialNumber0 AS [SerialNumber]
 FROM 
-	dbo.vWorkstationStatus INNER JOIN
-	dbo.v_R_System ON dbo.vWorkstationStatus.ResourceID = dbo.v_R_System.ResourceID LEFT OUTER JOIN
-	dbo.v_GS_SYSTEM_ENCLOSURE ON dbo.vWorkstationStatus.ResourceID = dbo.v_GS_SYSTEM_ENCLOSURE.ResourceID LEFT OUTER JOIN
-	dbo.v_GS_COMPUTER_SYSTEM ON dbo.vWorkstationStatus.ResourceID = dbo.v_GS_COMPUTER_SYSTEM.ResourceID LEFT OUTER JOIN
-	dbo.v_GS_OPERATING_SYSTEM ON dbo.vWorkstationStatus.ResourceID = dbo.v_GS_OPERATING_SYSTEM.ResourceID
+	dbo.v_R_System 
+	LEFT OUTER JOIN
+    dbo.v_GS_SYSTEM_ENCLOSURE ON dbo.v_R_System.ResourceID = dbo.v_GS_SYSTEM_ENCLOSURE.ResourceID 
+	LEFT OUTER JOIN
+    dbo.v_GS_OPERATING_SYSTEM ON dbo.v_R_System.ResourceID = dbo.v_GS_OPERATING_SYSTEM.ResourceID 
+	LEFT OUTER JOIN
+    dbo.v_GS_COMPUTER_SYSTEM ON dbo.v_R_System.ResourceID = dbo.v_GS_COMPUTER_SYSTEM.ResourceID 
+	LEFT OUTER JOIN
+    dbo.vWorkstationStatus ON dbo.v_R_System.ResourceID = dbo.vWorkstationStatus.ResourceID
 "@
 $query = $queryBase
 if ($ComputerNames.Count -gt 1) {
     $complist = ($ComputerNames | %{"'$_'"}) -join ','
-    $query += " WHERE (dbo.vWorkstationStatus.Name IN ($complist)) ORDER BY dbo.vWorkstationStatus.Name"
+    $query += " WHERE (dbo.v_R_System.Name0 IN ($complist)) ORDER BY dbo.vWorkstationStatus.Name"
 }
 else {
-    $query += " WHERE (dbo.vWorkstationStatus.Name = '$ComputerNames')"
+    $query += " WHERE (dbo.v_R_System.Name0 = '$ComputerNames')"
 }
-Write-Verbose $query
+#Write-Verbose $query
 .\Get-CMSQLQueryData.ps1 -Query $query -SQLServerName $ServerName -SiteCode $SiteCode
