fb554a0bb113ebc045b6ecdc30b7d3ea537126ae
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-11-30 16:23:29 -0500

    Create Software-Google-Chrome.sql

---
+++ queries/Software-Google-Chrome.sql
@@ -0,0 +1,15 @@
+SELECT DISTINCT 
+  dbo.v_R_System.Name0 AS ComputerName, 
+  dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED.ResourceID, 
+  dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED.ProductName0 AS ProductName, 
+  dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED.ARPDisplayName0 AS DisplayName, 
+  dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED.ProductVersion0 AS Version, 
+  dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED.Publisher0 AS Publisher, 
+  dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED.ProductCode0 AS ProductCode
+FROM 
+  dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED INNER JOIN
+  dbo.v_R_System ON dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED.ResourceID = dbo.v_R_System.ResourceID
+WHERE 
+  (dbo.v_GS_INSTALLED_SOFTWARE_CATEGORIZED.ProductName0 LIKE 'Google Chrome%')
+ORDER BY 
+  ComputerName
