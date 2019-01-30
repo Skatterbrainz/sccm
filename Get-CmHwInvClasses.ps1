f2a87e7102523eea007b1a2c8456aeda1c7e64a6
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-01-30 17:20:06 -0500

    Create Get-CmHwInvClasses.ps1

---
+++ Get-CmHwInvClasses.ps1
@@ -0,0 +1,37 @@
+Import-Module dbatools
+
+function Get-CmHwInvClasses {
+    param (
+      [parameter(Mandatory=$True)]
+      [ValidateNotNullOrEmpty()]
+      [string] $SQLServerName,
+      [parameter(Mandatory=$True)]
+      [ValidateLength(3)]
+      [string] $SiteCode
+    )
+    try {
+      $query = 'SELECT [Namespace],ClassName,SMSClassID,SMSGroupName,Properties 
+      FROM v_InventoryClass ORDER BY NameSpace, ClassName, SMSClassID'
+      $dataset = Invoke-DbaQuery -SqlInstance $SQLServerName -Database "CM_$SiteCode" -Query $query -ErrorAction Stop
+      $dataset | Foreach-Object {
+          $ns = $_.Namespace
+          $cn = $_.ClassName
+          $gn = $_.SMSGroupName
+          [xml]$p  = $_.Properties
+          $plist = @($p.Properties.Property | Foreach-Object {$_.PropertyName})
+          $px = [ordered]@{
+              Namespace = $ns
+              ClassName = $cn
+              GroupName = $gn
+              Properties = $px
+          }
+          New-Object PSObject -Property $px
+      }
+    }
+    catch {
+      Write-Error "Error: $($Error[0].Exception.Message)"
+    }
+}
+
+$classdata = Get-CmHwInvClasses
+$classdata | Format-List
