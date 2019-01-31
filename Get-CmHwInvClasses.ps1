f021c93aecb95ad26a9600720c3e2f532dc4a9ac
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-01-31 09:47:24 -0500

    Fixed Bad Query
    Thanks to Cody Mathis! #slack #windowsadminschannel

--- Get-CmHwInvClasses.ps1
+++ Get-CmHwInvClasses.ps1
@@ -1,37 +1,40 @@
 Import-Module dbatools
 
 function Get-CmHwInvClasses {
+    [CmdletBinding()]
     param (
       [parameter(Mandatory=$True)]
       [ValidateNotNullOrEmpty()]
       [string] $SQLServerName,
       [parameter(Mandatory=$True)]
-      [ValidateLength(3)]
+      [ValidateLength(3,3)]
       [string] $SiteCode
     )
+    $output = ""
     try {
-      $query = 'SELECT [Namespace],ClassName,SMSClassID,SMSGroupName,Properties 
-      FROM v_InventoryClass ORDER BY NameSpace, ClassName, SMSClassID'
-      $dataset = Invoke-DbaQuery -SqlInstance $SQLServerName -Database "CM_$SiteCode" -Query $query -ErrorAction Stop
-      $dataset | Foreach-Object {
-          $ns = $_.Namespace
-          $cn = $_.ClassName
-          $gn = $_.SMSGroupName
-          [xml]$p  = $_.Properties
-          $plist = @($p.Properties.Property | Foreach-Object {$_.PropertyName})
-          $px = [ordered]@{
-              Namespace = $ns
-              ClassName = $cn
-              GroupName = $gn
-              Properties = $px
-          }
-          New-Object PSObject -Property $px
-      }
+        $query = "SELECT DISTINCT irc.ClassName, irc.PropertyName, irc.SMSClassID, 
+        CASE WHEN (cip.SettingName IS NOT NULL) THEN cip.SettingName 
+        ELSE 'Default Client Settings'
+        END AS 'SettingName',
+        CASE WHEN cip.CollectionID IS NOT NULL THEN cip.CollectionID 
+        ELSE 'SMS00001' END AS 'CollectionID'
+        FROM v_InventoryReportClass irc
+            LEFT JOIN v_InventoryClassProperty icp ON icp.SMSClassID = irc.SMSClassID
+            LEFT JOIN v_CustomInventoryReport cip ON cip.InventoryReportID = irc.InventoryReportID
+            ORDER BY SettingName, ClassName, PropertyName"
+        $dataset = @(Invoke-DbaQuery -SqlInstance $SQLServerName -Database "CM_$SiteCode" -Query $query -ErrorAction SilentlyContinue)
+        $dataset | ForEach-Object {
+            $props = [ordered]@{
+                SettingName  = $_.SettingName
+                ClassName    = $_.ClassName
+                PropertyName = $_.PropertyName
+                SMSClassID   = $_.SMSClassID
+                CollectionID = $_.CollectionID
+            }
+            New-Object PSObject -Property $props
+        }
     }
     catch {
-      Write-Error "Error: $($Error[0].Exception.Message)"
+        Write-Error "ERROR: $($Error[0].Exception.Message)"
     }
 }
-
-$classdata = Get-CmHwInvClasses
-$classdata | Format-List
