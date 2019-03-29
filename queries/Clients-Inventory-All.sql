d5e2fa2af34da40e5d84b81ce66c4d325950cd16
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-03-28 23:17:53 -0400

    Update and rename cm_client_data.sql to Clients-Inventory-All.sql

--- queries/Clients-Inventory-All.sql
+++ queries/Clients-Inventory-All.sql
@@ -11,14 +11,20 @@ SELECT DISTINCT
 	dbo.v_GS_NETWORK_ADAPTER_CONFIGURATION.MACAddress0 AS MACAddress, 
 	dbo.v_GS_NETWORK_ADAPTER_CONFIGURATION.DefaultIPGateway0 AS IPGateway, 
 	dbo.v_GS_PROCESSOR.Name0 AS Processor,
-	 dbo.vWorkstationStatus.OperatingSystem, 
-	dbo.vWorkstationStatus.LastHardwareScan
+	dbo.v_GS_X86_PC_MEMORY.TotalPhysicalMemory0 as Memory, 
+	dbo.vWorkstationStatus.OperatingSystem, 
+	dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 as OSBuild, 
+	dbo.vWorkstationStatus.LastHardwareScan,
+	dbo.vWorkstationStatus.LastPolicyRequest,
+	dbo.vWorkstationStatus.LastMPServerName  
 FROM 
 	dbo.v_R_System INNER JOIN
 	dbo.vWorkstationStatus ON dbo.v_R_System.ResourceID = dbo.vWorkstationStatus.ResourceID LEFT OUTER JOIN
 	dbo.v_GS_PROCESSOR ON dbo.v_R_System.ResourceID = dbo.v_GS_PROCESSOR.ResourceID LEFT OUTER JOIN
 	dbo.v_GS_NETWORK_ADAPTER_CONFIGURATION ON dbo.v_R_System.ResourceID = dbo.v_GS_NETWORK_ADAPTER_CONFIGURATION.ResourceID LEFT OUTER JOIN
-	dbo.v_GS_LOGICAL_DISK ON dbo.v_R_System.ResourceID = dbo.v_GS_LOGICAL_DISK.ResourceID
+	dbo.v_GS_LOGICAL_DISK ON dbo.v_R_System.ResourceID = dbo.v_GS_LOGICAL_DISK.ResourceID LEFT OUTER JOIN 
+	dbo.v_GS_OPERATING_SYSTEM ON dbo.v_R_System.ResourceID = dbo.v_GS_OPERATING_SYSTEM.ResourceID LEFT OUTER JOIN
+	dbo.v_GS_X86_PC_MEMORY ON dbo.v_R_System.ResourceID = dbo.v_GS_X86_PC_MEMORY.ResourceID 
 WHERE 
 	(dbo.v_GS_LOGICAL_DISK.DeviceID0 = N'C:') 
 	AND 
