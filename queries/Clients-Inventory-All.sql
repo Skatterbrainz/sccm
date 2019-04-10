cd429b743b1983468a1417d59cbfdf699a536d7c
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-04-10 00:52:44 -0400

    serial number
    added system enclosure serialnumber

--- queries/Clients-Inventory-All.sql
+++ queries/Clients-Inventory-All.sql
@@ -14,17 +14,26 @@ SELECT DISTINCT
 	dbo.v_GS_X86_PC_MEMORY.TotalPhysicalMemory0 as Memory, 
 	dbo.vWorkstationStatus.OperatingSystem, 
 	dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 as OSBuild, 
+	dbo.v_GS_SYSTEM_ENCLOSURE.SerialNumber0 as SerialNum,
 	dbo.vWorkstationStatus.LastHardwareScan,
 	dbo.vWorkstationStatus.LastPolicyRequest,
 	dbo.vWorkstationStatus.LastMPServerName  
 FROM 
 	dbo.v_R_System INNER JOIN
-	dbo.vWorkstationStatus ON dbo.v_R_System.ResourceID = dbo.vWorkstationStatus.ResourceID LEFT OUTER JOIN
-	dbo.v_GS_PROCESSOR ON dbo.v_R_System.ResourceID = dbo.v_GS_PROCESSOR.ResourceID LEFT OUTER JOIN
-	dbo.v_GS_NETWORK_ADAPTER_CONFIGURATION ON dbo.v_R_System.ResourceID = dbo.v_GS_NETWORK_ADAPTER_CONFIGURATION.ResourceID LEFT OUTER JOIN
-	dbo.v_GS_LOGICAL_DISK ON dbo.v_R_System.ResourceID = dbo.v_GS_LOGICAL_DISK.ResourceID LEFT OUTER JOIN 
-	dbo.v_GS_OPERATING_SYSTEM ON dbo.v_R_System.ResourceID = dbo.v_GS_OPERATING_SYSTEM.ResourceID LEFT OUTER JOIN
-	dbo.v_GS_X86_PC_MEMORY ON dbo.v_R_System.ResourceID = dbo.v_GS_X86_PC_MEMORY.ResourceID 
+	dbo.vWorkstationStatus ON 
+	dbo.v_R_System.ResourceID = dbo.vWorkstationStatus.ResourceID LEFT OUTER JOIN
+	dbo.v_GS_PROCESSOR ON 
+	dbo.v_R_System.ResourceID = dbo.v_GS_PROCESSOR.ResourceID LEFT OUTER JOIN
+	dbo.v_GS_NETWORK_ADAPTER_CONFIGURATION ON 
+	dbo.v_R_System.ResourceID = dbo.v_GS_NETWORK_ADAPTER_CONFIGURATION.ResourceID LEFT OUTER JOIN
+	dbo.v_GS_LOGICAL_DISK ON 
+	dbo.v_R_System.ResourceID = dbo.v_GS_LOGICAL_DISK.ResourceID LEFT OUTER JOIN 
+	dbo.v_GS_OPERATING_SYSTEM ON 
+	dbo.v_R_System.ResourceID = dbo.v_GS_OPERATING_SYSTEM.ResourceID LEFT OUTER JOIN
+	dbo.v_GS_X86_PC_MEMORY ON 
+	dbo.v_R_System.ResourceID = dbo.v_GS_X86_PC_MEMORY.ResourceID LEFT OUTER JOIN
+	dbo.v_GS_SYSTEM_ENCLOSURE ON 
+	dbo.v_R_System.ResourceID = dbo.v_GS_SYSTEM_ENCLOSURE.ResourceID
 WHERE 
 	(dbo.v_GS_LOGICAL_DISK.DeviceID0 = N'C:') 
 	AND 
