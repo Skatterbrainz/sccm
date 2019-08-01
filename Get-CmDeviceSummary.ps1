e9b2bbe11e6d637d5f06fdf7040889fc2f1f47ef
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-08-01 12:54:53 -0400

    Update Get-CmDeviceSummary.ps1

--- Get-CmDeviceSummary.ps1
+++ Get-CmDeviceSummary.ps1
@@ -2,10 +2,32 @@ param (
   [string] $SiteServer,
   [string] $SiteCode
 )
+$smsns = "root\sms\site_$SiteCode"
+
 $q1 = "select resourceid,name,adsitename,client,isvirtualmachine,lastlogontimestamp from sms_r_system"
 $q2 = "select resourceid,name,manufacturer,model from sms_g_system_computer_system"
+$q3 = "select ipsubnets,name from sms_r_system"
 
-$x1 = Get-CimInstance -Namespace "root\sms\site_$SiteCode" -ComputerName $SiteServer -Query $q1 | 
-    select resourceid,name,adsitename,client,isvirtualmachine,lastlogontimestamp
-$x2 = Get-CimInstance -Namespace "root\sms\site_$SiteCode" -ComputerName $SiteServer -Query $q2 | 
-    select resourceid,name,manufacturer,model
+# get all devices
+$x1 = Get-CimInstance -Namespace $smsns -ComputerName $SiteServer -Query $q1 | 
+  select resourceid,name,adsitename,client,isvirtualmachine,lastlogontimestamp
+
+# get clients with inventory data
+$x2 = Get-CimInstance -Namespace $smsns -ComputerName $SiteServer -Query $q2 | 
+  select resourceid,name,manufacturer,model
+
+# get unique device IP subnets
+$x3 = Get-CimInstance -Namespace $smsns -ComputerName $SiteServer -Query $q3 | select ipsubnets,name
+$x4 = $x3 | % {
+    try {
+      $ipsub = @($_.IPSubnets | ?{ !$_.StartsWith('169') })[0]
+    }
+    catch {
+      $ipsub = $null
+    }
+    [pscustomobject]@{
+        Name = $_.Name
+        Subnet = $ipsub
+    }
+}
+$unique_subnets = $x4 | select subnet -Unique
