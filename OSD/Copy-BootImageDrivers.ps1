835f72e8749300711d9e9769fb984aa37f8eef1d
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-08-06 10:29:06 -0400

    added exception handling, params

--- OSD/Copy-BootImageDrivers.ps1
+++ OSD/Copy-BootImageDrivers.ps1
@@ -1,25 +1,37 @@
-function Get-CmAdminConsolePath {
-	$result = Get-ItemProperty "HKLM:SOFTWARE\Microsoft\SMS\Setup" | 
-		Select-Object -ExpandProperty "UI Installation Directory"
+[CmdletBinding()]
+param (
+	[parameter(Mandatory)] [ValidateNotNullOrEmpty()] [string] $SiteCode
+)
+
+function Get-CmPsModulePath {
+	param()
+	$result = Get-ItemProperty "HKLM:SOFTWARE\Microsoft\SMS\Setup" | Select-Object -ExpandProperty "UI Installation Directory"
 	$cpath = "$result\bin\ConfigurationManager.psd1"
 	if (Test-Path $cpath) { $cpath }
 }
 
-Function Copy-BootImageDrivers {
-    PARAM (
-        $from, $to
+function Copy-BootImageDrivers {
+    param (
+        [parameter(Mandatory)] [ValidateNotNullOrEmpty()] [string] $SourcePath, 
+		[parameter(Mandatory)] [ValidateNotNullOrEmpty()] [string] $TargetPath
     )
- 
-    $boot = Get-CMBootImage -ID $to
- 
-    (Get-CMBootImage -Id $from).ReferencedDrivers | ForEach-Object {
-        Write-Verbose "Copying $($_.Id) to $($to)"
+    $boot = Get-CMBootImage -Id $TargetPath
+    (Get-CMBootImage -Id $SourcePath).ReferencedDrivers | ForEach-Object {
+        Write-Verbose "Copying $($_.Id) to $($TargetPath)"
         Set-CMDriver -Id $_.Id -AddBootImagePackage $boot -UpdateDistributionPointsforBootImagePackage $false
     }
- 
 }
 
-Import-Module $(Get-CmAdminConsolePath)
-CD PS1: 
-#Example use
-Copy-BootImageDrivers -from "ABC00123" -to "ABC00456"
+try {
+	Import-Module $(Get-CmPsModulePath)
+	$oldloc = Get-Location
+	Set-Location "$SiteCode`:"
+	#Example use
+	Copy-BootImageDrivers -from "ABC00123" -to "ABC00456"
+}
+catch {
+	Write-Error $Error[0].Exception.Message
+}
+finally {
+	Set-Location $oldloc
+}
