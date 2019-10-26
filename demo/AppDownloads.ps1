adb713fe821b416b252a1ca7c9ece492ab22acd5
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-10-26 11:40:01 -0400

    Create AppDownloads.ps1

---
+++ demo/AppDownloads.ps1
@@ -0,0 +1,35 @@
+[CmdletBinding()]
+param(
+    [parameter()][ValidateNotNullOrEmpty()][string]$InputFile = "I:\scripts\appdownloads.txt",
+    [parameter()][ValidateNotNullOrEmpty()][string]$rootPath = "H:\SOURCES"
+)
+try {
+    $apps = Get-Content $InputFile
+    foreach ($app in $apps) {
+        $appdata   = $app -split '~'
+        $appPath   = $appdata[0]
+        $appSource = $appdata[1]
+        if ($appdata.Count -eq 3) {
+            $filename = $appdata[2]
+        }
+        else {
+            $filename  = $($appSource -split '/' | select -Last 1) -replace '%20','_'
+        }
+        $destPath  = Join-Path -Path $rootPath -ChildPath $appPath
+        if (!(Test-Path $destPath)) { 
+            mkdir $destPath -Force 
+            Write-Verbose "created folder: $destPath"
+        }
+        $destination = Join-Path -Path $destPath -ChildPath "$filename"
+        if (!(Test-Path $destination)) {
+            Invoke-WebRequest -Uri $appSource -OutFile $destination
+        }
+        else {
+            Write-Verbose "file exists: $destination"
+        }
+    }
+    Write-Host "finished processing"
+}
+catch {
+    Write-Error $_.Exception.Message 
+}
