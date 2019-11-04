8c86793db449c99755ba03675665d997ddc749cd
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-11-04 12:50:00 -0500

    new script

---
+++ OSD/Set-ComputerNameX.ps1
@@ -0,0 +1,71 @@
+[CmdletBinding()]
+param (
+    [parameter()][ValidateSet('Serial','Form-Serial')][string] $Format = 'Serial',
+    [parameter()][ValidateSet('D','DT','W','WS')][string] $WorkstationPrefix = 'WS',
+    [parameter()][ValidateSet('L','LT')][string] $LaptopPrefix = 'LT',
+    [parameter()][int] $NameLength = 8,
+    [parameter()][ValidateSet('Left','Right')][string] $TrimSerialFrom
+)
+
+function Get-FormFactorCode {
+	[CmdletBinding()]
+    param ()
+    $cff = $(Get-CimInstance -ClassName Win32_SystemEnclosure -Namespace root\cimv2).ChassisTypes
+	# ignore docks/port replicators which return an array rather than one value
+    if ($cff.Count -gt 1) {
+        $cff = $cff[0]
+        Write-Verbose "*** filtering out duplicate for external device"
+    }
+    Write-Verbose "*** chassis type = $cff"
+    # as of Q1-2018, codes are from 1 to 36
+    # see https://blogs.technet.microsoft.com/brandonlinton/2017/09/15/updated-win32_systemenclosure-chassis-types/
+    # desktops
+    if ($cff -in (3..7)+(13,34,35)) { $ff = $WorkstationPrefix }
+    # laptops
+    elseif ($cff -in (10,11,12,14)+(15..30)+(31,32,33,36)) { $ff = $LaptopPrefix }
+    # servers, but whatever, I do what I want!
+    elseif ($cff -in (17..24)) { $ff = $WorkstationPrefix }
+    # unknown
+    else { $ff = $WorkstationPrefix }
+    Write-Output $ff
+}
+
+try {
+    $result = ""
+    # get serial number
+    $sn = $(Get-CimInstance -ClassName Win32_SystemEnclosure -Namespace root\cimv2).SerialNumber
+    Write-Verbose "*** serial number... $sn"
+    # get chassis type
+    if ($sn.Length -gt $NameLength) {
+        if ($TrimSerialFrom -eq 'Left') {
+            $curlen = $sn.Length
+            $sn = $sn.Substring($NameLength - $curlen-1, $NameLength)
+        }
+        else {
+            $sn = $sn.Substring(0, $NameLength)
+        }
+    }
+    Write-Verbose "*** trimmed value... $sn"
+    if ($Format -eq 'Serial') {
+        $result = $sn
+    }
+    else {
+        $pfx = Get-FormFactorCode -Verbose:$VerbosePreference
+        $result = "$pfx`-$sn"
+    }
+    try {
+        $tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
+        #$tsui  = New-Object -COMObject Microsoft.SMS.TSProgressUI
+        Write-Verbose "*** assigning OSDComputerName to $result"
+        $tsenv.Value("OSDComputerName") = $result
+    }
+    catch {
+        Write-Verbose "*** not running in a task sequence environment"
+    }
+}
+catch {
+    Write-Error $_.Exception.Message 
+}
+finally {
+    Write-Output $result
+}
