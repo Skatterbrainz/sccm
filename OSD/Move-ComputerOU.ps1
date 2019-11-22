5a82aff9d6b0c799c328c3b3f09b4f2914a3967b
skatterbrainz <dstein2017@gmail.com>
2019-11-21 22:46:23 -0500

    updated parameters and comments

--- OSD/Move-ComputerOU.ps1
+++ OSD/Move-ComputerOU.ps1
@@ -5,8 +5,6 @@
     Yeah, what he just said
 .PARAMETER OU
     LDAP path to move account into
-.PARAMETER TSVariable
-    Name of custom TS variable to use in place of $OU
 .PARAMETER ComputerName
     Name of computer account. Default is $env:COMPUTERNAME
 .EXAMPLE
@@ -18,38 +16,27 @@
 .EXAMPLE
     Move-ComputerOU.ps1 -OU %MachinObjectOU%
     Move local computer account to OU specified by OSD task sequence variable
-.EXAMPLE
-    Move-ComputerOU.ps1 -TSVariable "MyOUPath"
-    Move local computer account to OU specified by OSD task sequence variable "MyOUPath"
 .NOTES
     Adapted from https://ccmexec.com/2018/03/move-the-computer-to-the-correct-ou-during-osd-ps-version/
-    Updated 1911.21 to allow empty -OU and specify -TSVariable
 #>
 [CmdletBinding()]
 param (
-    [parameter(Position=1)][string] $OU = "",
-    [parameter()][string] $TSVariable = "",
-    [parameter()][string] $ComputerName = $($env:COMPUTERNAME)
+    [parameter()][string] $OU = "",
+    [parameter()][string] $ComputerName = $($env:COMPUTERNAME),
+    [parameter()][string] $DefaultOU = 'OU=DisabledComputers,OU=CORP,DC=contoso,DC=local'
 )
+$ErrorActionPreference = 'stop'
+if ([string]::IsNullOrEmpty($OU)) {
+    Write-Verbose "*** path not specified / using default path"
+    $OU = $DefaultOU
+}
 try {
-    Write-Verbose "*** searching for computer $ComputerName"
     $CompDN = ([ADSISEARCHER]"sAMAccountName=$ComputerName`$").FindOne().Path
-    Write-Verbose "*** computer account found in directory: $ComputerName"
+    Write-Verbose "*** computer account found in directory: $env:COMPUTERNAME"
     $CompObj = [ADSI]"$CompDN"
-    if (![string]::IsNullOrEmpty($TSVariable)) {
-        Write-Verbose "*** reading ou path from variable $TSVariable"
-        $tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment -ErrorAction SilentlyContinue
-        $OU = $($tsenv.Value("$TSVariable")).ToString().Trim()
-    }
-    Write-Verbose "*** path = $OU"
-    if (![string]::IsNullOrEmpty($OU)) {
-        $CompObj.psbase.MoveTo([ADSI]"LDAP://$($OU)")
-        Write-Verbose "*** computer account has been moved"
-        Write-Output 0    
-    }
-    else {
-        throw "ou path was not specified or found"
-    }
+    $CompObj.psbase.MoveTo([ADSI]"LDAP://$($OU)")
+    Write-Verbose "*** computer account has been moved"
+    Write-Output 0
 }
 catch {
     $_.Exception.Message ; Exit 1
